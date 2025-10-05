a1c3bef6ee7c6fbe24403fb62bdd1491e0a7a3a9
thyrogi <contasecundariasla1oo@gmail.com>
2025-10-05 01:13:22 -0300

    Add files via upload

---
+++ css/js/script.js
@@ -0,0 +1,42 @@
+function playJoke() {
+            const audio = document.getElementById("rickrolled");
+            const quoteBox = document.getElementById("quote-box");
+            
+            audio.play();
+            quoteBox.innerText = "rick rolado - thyrogi";
+
+            audio.onended = () => {
+            randomQuote();
+            }
+        }
+        
+        function randomQuote() {
+            const quotes = [
+                "nois dá o cu porra - thyrogi",
+                "*inserir frase legal aqui* - thyrogi",
+                "os predadores de perereca venceram - thyrogi",
+                "e mais uma vez, a homofobia vence – thyrogi",
+                "pqq todo mundo que eu amo é pedófilo :( - thyrogi",
+                "só pq ta certo n quer dizer q ta errado - thyrogi",
+                "PEGA NO MEU EGOISTA! - thyrogi",
+                "AAAAAAAAAAAAAAAAAAAAAAAAAAAAA - thyrogi",
+                "eita - thyrogi",
+                "zzzzz - thyrogi"
+            ];
+        
+            const theQuote = quotes[Math.floor(Math.random() * quotes.length)];
+        
+            document.getElementById("quote-box").innerText = theQuote;
+
+            if (theQuote === "rick rolado - thyrogi") {
+                
+            }
+        }
+        randomQuote();
+        document.getElementById("quote-box").onclick = function() {
+            const audio = document.getElementById("rickrolled");
+            if (audio.paused) {
+                randomQuote();
+            }
+        };
+            
